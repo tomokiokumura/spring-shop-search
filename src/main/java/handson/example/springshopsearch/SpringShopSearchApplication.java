d6f7057e3819801bba0b788fcc5a9277861a51e6
Takahito Hashimoto <ta.li.ryu.1994@gmail.com>
2018-10-10 20:23:08 +0900

    Add project.

---
+++ src/main/java/handson/example/springshopsearch/SpringShopSearchApplication.java
@@ -0,0 +1,12 @@
+package handson.example.springshopsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringShopSearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringShopSearchApplication.class, args);
+    }
+}
