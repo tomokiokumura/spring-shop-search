7228b7119a8ddeded5fd41c81ea07090f02f6bfe
Takahito Hashimoto <ta.li.ryu.1994@gmail.com>
2018-10-14 18:46:56 +0900

    Remove user model.

--- src/main/java/handson/example/springshopsearch/model/user/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package handson.example.springshopsearch.model.user;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findFirstByName(String name);
-}
