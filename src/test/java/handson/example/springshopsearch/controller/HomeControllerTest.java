f7101d62a3f1ab9760526a7ca50566303534ad1b
Takahito Hashimoto <ta.li.ryu.1994@gmail.com>
2018-10-19 07:21:12 +0900

    Add HomeControllerTest.

---
+++ src/test/java/handson/example/springshopsearch/controller/HomeControllerTest.java
@@ -0,0 +1,36 @@
+package handson.example.springshopsearch.controller;
+
+import org.hamcrest.Matchers;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HomeControllerTest {
+
+    MockMvc mockMvc;
+
+    @Autowired
+    HomeController homeController;
+
+    @Before
+    public void setup() {
+        mockMvc = MockMvcBuilders.standaloneSetup(homeController).build();
+    }
+
+    @Test
+    public void index() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/"))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+}
