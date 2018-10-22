498a9eaee8b6531686c3cdf456e384d4900e0da6
Takahito Hashimoto <takahito_hashimoto@unirita.co.jp>
2018-10-22 11:25:47 +0900

    Fix

--- src/main/java/handson/example/springshopsearch/controller/HomeController.java
+++ src/main/java/handson/example/springshopsearch/controller/HomeController.java
@@ -8,6 +8,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/")
 public class HomeController {
 
+    @GetMapping
+    public String getIndex() {
+        return "index";
+    }
+
     @GetMapping("about")
     public String getAbout() {
         return "about";
